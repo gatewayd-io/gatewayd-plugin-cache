3071d8d9aa18f3d6162e99472a41c3bf396a4e32
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 22:08:00 +0100

    Update protobuf and grpc stubs

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -6,14 +6,14 @@ import (
 
 	"github.com/eko/gocache/lib/v4/cache"
 	"github.com/eko/gocache/store/redis/v4"
-	plugin_v1 "github.com/gatewayd-io/gatewayd-plugin-cache/plugin/v1"
+	pluginV1 "github.com/gatewayd-io/gatewayd-plugin-cache/plugin/v1"
 	"github.com/hashicorp/go-hclog"
 	goplugin "github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-var PluginID = plugin_v1.PluginID{
+var PluginID = pluginV1.PluginID{
 	Name:      "gatewayd-plugin-cache",
 	Version:   "0.0.1",
 	RemoteUrl: "github.com/gatewayd-io/gatewayd-plugin-cache",
@@ -25,7 +25,7 @@ var PluginMap = map[string]goplugin.Plugin{
 
 type Plugin struct {
 	goplugin.GRPCPlugin
-	plugin_v1.GatewayDPluginServiceServer
+	pluginV1.GatewayDPluginServiceServer
 	Logger     hclog.Logger
 	RedisStore *redis.RedisStore
 }
@@ -45,13 +45,13 @@ func NewCachePlugin(impl Plugin) *CachePlugin {
 
 // GRPCServer registers the plugin with the gRPC server.
 func (p *CachePlugin) GRPCServer(b *goplugin.GRPCBroker, s *grpc.Server) error {
-	plugin_v1.RegisterGatewayDPluginServiceServer(s, &p.Impl)
+	pluginV1.RegisterGatewayDPluginServiceServer(s, &p.Impl)
 	return nil
 }
 
 // GRPCClient returns the plugin client.
 func (p *CachePlugin) GRPCClient(ctx context.Context, b *goplugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
-	return plugin_v1.NewGatewayDPluginServiceClient(c), nil
+	return pluginV1.NewGatewayDPluginServiceClient(c), nil
 }
 
 // GetPluginConfig returns the plugin config.
