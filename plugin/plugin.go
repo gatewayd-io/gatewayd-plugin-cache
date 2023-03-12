7c9c3d44358f2c77472140cee6132ffe74cef949
Mostafa Moradian <mostafa@gatewayd.io>
2023-03-12 01:05:39 +0100

    Lower the log level to debug, as this is not an actual error when the client is authenticating

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -256,9 +256,8 @@ func (p *Plugin) OnTrafficFromServer(
 	// This might also happen if the cache is cleared while the client is still connected.
 	// In this case, the client should reconnect and the error will go away.
 	if database == "" {
-		p.Logger.Error(
-			"Database name not found or set in cache, startup message or plugin config. Skipping cache")
-		p.Logger.Error("Consider setting the database name in the plugin config or disabling the plugin if you don't need it")
+		p.Logger.Debug("Database name not found or set in cache, startup message or plugin config. Skipping cache")
+		p.Logger.Debug("Consider setting the database name in the plugin config or disabling the plugin if you don't need it")
 		return resp, nil
 	}
 
