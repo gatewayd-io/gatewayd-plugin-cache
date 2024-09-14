ef9e007eec440bd25369e88e67e74299f0291763
Mostafa Moradian <mostafa@gatewayd.io>
2024-09-14 14:45:01 +0200

    Refactor and update tests to reflect changes

--- plugin/api_client_test.go
+++ plugin/api_client_test.go
@@ -1,19 +1,20 @@
 package plugin
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 
+	apiV1 "github.com/gatewayd-io/gatewayd/api/v1"
 	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/assert"
 	"github.com/zenizh/go-capturer"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-// Test_Plugin_getProxies_Fails_APIAddress tests that getProxies() fails when APIAddress is not set.
-func Test_Plugin_getProxies_Fails_APIAddress(t *testing.T) {
+// Test_Plugin_getProxies_Fails_APIClient tests that getProxies()
+// fails when the API client is not initialized.
+func Test_Plugin_getProxies_Fails_APIClient(t *testing.T) {
 	output := capturer.CaptureOutput(func() {
 		p := Plugin{
 			Logger: hclog.New(&hclog.LoggerOptions{
@@ -26,127 +27,29 @@ func Test_Plugin_getProxies_Fails_APIAddress(t *testing.T) {
 		assert.Nil(t, proxies)
 	})
 
-	assert.Contains(t, output, "[ERROR] test: Failed to get a list of proxies from GatewayD: error=\"APIAddress is not set\"\n")
+	assert.Contains(t, output, "[ERROR] test: Failed to get a list of proxies from GatewayD: error=\"API client is not initialized\"\n")
 }
 
-// Test_Plugin_getProxies_Fails_Request_Error tests that getProxies() fails when the request fails.
-func Test_Plugin_getProxies_Fails_Request_Error(t *testing.T) {
+// Test_Plugin_getProxies_Fails_Connection_Error tests that getProxies()
+// fails when the API client fails to connect to GatewayD.
+func Test_Plugin_getProxies_Fails_Connection_Error(t *testing.T) {
 	output := capturer.CaptureOutput(func() {
+		conn, err := grpc.NewClient(
+			"localhost:18080",
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		assert.NoError(t, err)
 		p := Plugin{
 			Logger: hclog.New(&hclog.LoggerOptions{
 				Name:   "test",
 				Level:  hclog.Trace,
 				Output: os.Stdout,
 			}),
-			APIAddress: "localhost:12345",
+			APIClient: apiV1.NewGatewayDAdminAPIServiceClient(conn),
 		}
 		proxies := p.getProxies()
 		assert.Nil(t, proxies)
 	})
 
-	assert.Contains(t, output, "[ERROR] test: Failed to get a list of proxies from GatewayD: error=\"Get \\\"http://localhost:12345/v1/GatewayDPluginService/GetProxies\\\": dial tcp [::1]:12345: connect: connection refused\"\n")
-}
-
-// Test_Plugin_getProxies_Fails_Inaccessible tests that getProxies() fails
-// when the API is not accessible.
-func Test_Plugin_getProxies_Fails_Inaccessible(t *testing.T) {
-	output := capturer.CaptureOutput(func() {
-		p := Plugin{
-			Logger: hclog.New(&hclog.LoggerOptions{
-				Name:   "test",
-				Level:  hclog.Trace,
-				Output: os.Stdout,
-			}),
-			APIAddress: "127.0.0.1:18080",
-		}
-		proxies := p.getProxies()
-		assert.Nil(t, proxies)
-	})
-
-	assert.Contains(t, output, "[ERROR] test: Failed to get a list of proxies from GatewayD: error=\"Get \\\"http://127.0.0.1:18080/v1/GatewayDPluginService/GetProxies\\\": dial tcp 127.0.0.1:18080: connect: connection refused\"\n")
-}
-
-// Test_Plugin_getProxies_Fails_Invalid_JSON tests that getProxies() fails
-// when the response is not valid JSON.
-func Test_Plugin_getProxies_Fails_Invalid_JSON(t *testing.T) {
-	output := capturer.CaptureOutput(func() {
-		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			if _, err := w.Write([]byte(``)); err != nil {
-				t.Fatal(err)
-			}
-		}))
-		defer testServer.Close()
-
-		p := Plugin{
-			Logger: hclog.New(&hclog.LoggerOptions{
-				Name:   "test",
-				Level:  hclog.Trace,
-				Output: os.Stdout,
-			}),
-			APIAddress: strings.TrimPrefix(testServer.URL, "http://"),
-		}
-
-		proxies := p.getProxies()
-		assert.Nil(t, proxies)
-	})
-
-	assert.Contains(t, output, "[ERROR] test: Failed to decode response from GatewayD: error=EOF\n")
-}
-
-// Test_Plugin_getProxies_Fails_Empty_Response tests that getProxies() returns an empty map
-// when the response is empty.
-func Test_Plugin_getProxies_Fails_Empty_Response(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		if _, err := w.Write([]byte(`{}`)); err != nil {
-			t.Fatal(err)
-		}
-	}))
-	defer testServer.Close()
-
-	p := Plugin{
-		Logger: hclog.New(&hclog.LoggerOptions{
-			Name:   "test",
-			Level:  hclog.Trace,
-			Output: os.Stdout,
-		}),
-		APIAddress: strings.TrimPrefix(testServer.URL, "http://"),
-	}
-
-	proxies := p.getProxies()
-	assert.NotNil(t, proxies)
-	assert.Equal(t, 0, len(proxies))
-	assert.Equal(t, proxies, map[string]Proxy{})
-}
-
-// Test_Plugin_getProxies tests that getProxies() returns a map of proxies.
-func Test_Plugin_getProxies(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		if _, err := w.Write([]byte(`{"default":{"available":["localhost:45121"],"busy":["localhost:45123"],"total":2}}`)); err != nil {
-			t.Fatal(err)
-		}
-	}))
-	defer testServer.Close()
-
-	p := Plugin{
-		Logger: hclog.New(&hclog.LoggerOptions{
-			Name:   "test",
-			Level:  hclog.Trace,
-			Output: os.Stdout,
-		}),
-		APIAddress: strings.TrimPrefix(testServer.URL, "http://"),
-	}
-
-	proxies := p.getProxies()
-	assert.Equal(t, 1, len(proxies))
-	assert.Equal(t, 1, len(proxies["default"].Available))
-	assert.Equal(t, 1, len(proxies["default"].Busy))
-	assert.Equal(t, 2, proxies["default"].Total)
-	assert.Equal(t, "localhost:45121", proxies["default"].Available[0])
-	assert.Equal(t, "localhost:45123", proxies["default"].Busy[0])
+	assert.Contains(t, output, `[ERROR] test: Failed to get a list of proxies from GatewayD: error="rpc error: code = Unavailable desc = connection error: desc = \"transport: Error while dialing: dial tcp 127.0.0.1:18080: connect: connection refused\""`)
 }
